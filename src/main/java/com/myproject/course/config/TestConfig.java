11534de6c032c5944370dd129394516fce0ca967
DSousaPr <sousaprm99@gmail.com>
2022-05-22 01:32:54 -0300

    Category

--- src/main/java/com/myproject/course/config/TestConfig.java
+++ src/main/java/com/myproject/course/config/TestConfig.java
@@ -7,9 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.myproject.course.entities.Category;
 import com.myproject.course.entities.Order;
 import com.myproject.course.entities.User;
 import com.myproject.course.entities.enums.OrderStatus;
+import com.myproject.course.repositories.CategoryRepository;
 import com.myproject.course.repositories.OrderRepository;
 import com.myproject.course.repositories.UserRepository;
 
@@ -22,8 +24,17 @@ public class TestConfig implements CommandLineRunner{
 	@Autowired
 	private OrderRepository orderRepository;
 	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Eletronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "9888888", "123456");
 		User u2 = new User(null, "Alex Green", "alexgreen@gmail.com", "9777777", "123456");
